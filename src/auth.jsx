a85903ac50fd18706559b1bd20c412ba18ae0a2a
Jackson-sch <darwinjackson.12@gmail.com>
2024-08-16 20:46:13 -0500

    getssesion

--- src/auth.jsx
+++ src/auth.jsx
@@ -42,13 +42,74 @@ export const { handlers, singIn, signOut, auth } = NextAuth({
         const user = await User.findOne({ email }).select("+password +role");
 
         if (!user) {
-          throw new Error("Email not found");
+          throw new Error("Invalid email or password");
         }
         if (!user.password) {
-          throw new Error("Password not found");
+          throw new Error("Invalid email or password");
         }
-        return user;
+
+        const isMatched = await compare(password, user.password);
+
+        if (!isMatched) {
+          throw new Error("Password does not match");
+        }
+
+        const userData = {
+          firstName: user.firstName,
+          lastName: user.lastName,
+          email: user.email,
+          role: user.role,
+          id: user._id,
+        };
+
+        return userData;
       },
     }),
   ],
+
+  pages: {
+    signIn: "/login",
+  },
+
+  callbacks: {
+    async session({ session, token }) {
+      if (token?.sub && token?.role) {
+        session.user.id = token.sub;
+        session.user.role = token.role;
+      }
+
+      return session;
+    },
+
+    async jwt({ token, user }) {
+      if (user) {
+        token.role = user.role;
+      }
+      return token;
+    },
+
+    signIn: async ({ user, account }) => {
+      if (account?.provider === "google") {
+        try {
+          const { email, name, image, id } = user;
+          await dbConnect();
+          const alreadyUser = await User.findOne({ email });
+
+          if (!alreadyUser) {
+            await User.create({ email, name, image, authProviderId: id });
+          } else {
+            return true;
+          }
+        } catch (error) {
+          throw new Error("Error while creating user");
+        }
+      }
+
+      if (account?.provider === "credentials") {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
 });
