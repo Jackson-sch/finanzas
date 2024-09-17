75217afc15e0e9ea6714089ce72914387711776c
Jackson-sch <darwinjackson.12@gmail.com>
2024-09-17 18:27:20 -0500

    Actualizar importación de página de inicio a página de inicio de sesión

--- src/app/(routes)/page.jsx
+++ src/app/(routes)/page.jsx
@@ -1,12 +1,3 @@
-import NavbarHome from "@/components/Navbar/NavbarHome";
+import LoginPage from "../(auth)/login/page";
 
-export default function HomePage() {
-  return (
-    <div className="min-h-screen min-w-full">
-      <NavbarHome />
-      <div className="container mx-auto mt-8">
-        <h1>Soy la página de inicio</h1>
-      </div>
-    </div>
-  );
-}
+export default LoginPage
